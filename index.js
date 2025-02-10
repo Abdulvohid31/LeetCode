f92a8483c39663d86a6e4a163bb6aa1f164cf6d9
Abdulvohid31 <abdulvohidcode@gmail.com>
2025-02-10 13:05:08 +0500

    as

--- index.js
+++ index.js
@@ -1432,3 +1432,16 @@
 // };
 
 // console.log(areAlmostEqual("npv", "zpn")) 
+
+// Dayly 3174
+var clearDigits = function(s) {
+    let stack = [];
+    for (let char of s) {
+        if (/\d/.test(char)) {
+            if (stack.length > 0) stack.pop(); 
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join('');
+};
