fc1c552b8ffc8352a3ea8e62836471bb7467d559
abdulvohid31 <abdulvohidcode@gmail.com>
2025-03-14 13:50:17 +0500

    Array Wrapper

--- index.js
+++ index.js
@@ -1646,3 +1646,19 @@
 //     return obj;
 // };
 // console.log(compactObject([null, 0, false, 1, true]));
+
+// class ArrayWrapper {
+//     private nums: number[];
+
+//     constructor(nums: number[]) {
+//         this.nums = nums;
+//     }
+
+//     valueOf(): number {
+//         return this.nums.reduce((sum, num) => sum + num, 0);
+//     }
+
+//     toString(): string {
+//         return `[${this.nums.join(",")}]`;
+//     }
+// }
