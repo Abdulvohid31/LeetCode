e00748fe279d541529362d4d7b9fd386e19d9029
abdulvohid31 <abdulvohidcode@gmail.com>
2025-03-14 16:11:36 +0500

    qanqadur ybanski narsani push qildm

--- index.js
+++ index.js
@@ -1649,15 +1649,12 @@
 
 // class ArrayWrapper {
 //     private nums: number[];
-
 //     constructor(nums: number[]) {
 //         this.nums = nums;
 //     }
-
 //     valueOf(): number {
 //         return this.nums.reduce((sum, num) => sum + num, 0);
 //     }
-
 //     toString(): string {
 //         return `[${this.nums.join(",")}]`;
 //     }
