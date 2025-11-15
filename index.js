f208a5e95929635ad8816dcb0dc5973fa32fcb1f
abdulvohid31 <abdulvohidcode@gmail.com>
2025-11-15 14:44:44 +0500

    longestValidParentheses done

--- index.js
+++ index.js
@@ -1791,4 +1791,23 @@
 //   return ans;
 // };
 
-console.log(numberOfSubstrings("101101"));
+// 
+// var longestValidParentheses = function(s) {
+//     let maxLen = 0;
+//     const stack = [-1];
+//     for (let i = 0; i < s.length; i++) {
+//       if (s[i] === '(') {
+//         stack.push(i);
+//       } else {
+//         stack.pop();
+//         if (stack.length === 0) {
+//           stack.push(i);
+//         } else {
+//           maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+//         }
+//       }
+//     }
+//     return maxLen;
+//   };
+// console.log(longestValidParentheses(")()())"));
+  
